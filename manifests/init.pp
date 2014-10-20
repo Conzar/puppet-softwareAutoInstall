d43f58ead08204f2c09a26ad7addf9df2f7281cb
Maxime Schmitt <maxime.schmitt@telecom-bretagne.eu>
2014-10-20 11:45:41 +0200

    update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -40,7 +40,9 @@
 # [Remember: No empty lines between comments and class definition]
 #
 class softwareAutoInstall(
-    $ensure = $softwareAutoInstall::params::ensure
+    $ensure = $softwareAutoInstall::params::ensure,
+    $branch = "core",
+    $EasyBuildVersion = ""
 )
 inherits softwareAutoInstall::params
 {
@@ -77,107 +79,32 @@ class softwareAutoInstall::common {
     # package { $softwareAutoInstall::params::extra_packages:
     #     ensure => 'present'
     # }
+    Exec { path => $easybuild::params::path }
 
     if $softwareAutoInstall::ensure == 'present' {
+      exec { 'install':
+        user    => 'sw',
+        command => "bash -c 'cd /tmp && sh install.py ${softwareAutoInstall::branch} ${softwareAutoInstall::EasyBuildVersion} && rm -f install.py && rm -f softwares.yaml'",
+        umask   => '022',
+        require => [ File [ 'install.py' ], File [ 'softwares.yaml' ] ],
+      }
 
-        # Prepare the log directory
-        file { "${softwareAutoInstall::params::logdir}":
-            ensure => 'directory',
-            owner  => "${softwareAutoInstall::params::logdir_owner}",
-            group  => "${softwareAutoInstall::params::logdir_group}",
-            mode   => "${softwareAutoInstall::params::logdir_mode}",
-            require => Package['softwareAutoInstall'],
-        }
+      file { 'install.py':
+        ensure => present,
+        path   => '/tmp/install.py',
+        owner  => 'sw',
+        mode   => '0755',
+        source => 'puppet:///modules/softwareAutoInstall/install.py',
+      }
 
-        # Configuration file
-        # file { "${softwareAutoInstall::params::configdir}":
-        #     ensure => 'directory',
-        #     owner  => "${softwareAutoInstall::params::configdir_owner}",
-        #     group  => "${softwareAutoInstall::params::configdir_group}",
-        #     mode   => "${softwareAutoInstall::params::configdir_mode}",
-        #     require => Package['softwareAutoInstall'],
-        # }
-        # Regular version using file resource
-        file { 'softwareAutoInstall.conf':
-            path    => "${softwareAutoInstall::params::configfile}",
-            owner   => "${softwareAutoInstall::params::configfile_owner}",
-            group   => "${softwareAutoInstall::params::configfile_group}",
-            mode    => "${softwareAutoInstall::params::configfile_mode}",
-            ensure  => "${softwareAutoInstall::ensure}",
-            #content => template("softwareAutoInstall/softwareAutoInstallconf.erb"),
-            #source => "puppet:///modules/softwareAutoInstall/softwareAutoInstall.conf",
-            #notify  => Service['softwareAutoInstall'],
-            require => [
-                        #File["${softwareAutoInstall::params::configdir}"],
-                        Package['softwareAutoInstall']
-                        ],
-        }
-
-        # # Concat version
-        # include concat::setup
-        # concat { "${softwareAutoInstall::params::configfile}":
-        #     warn    => false,
-        #     owner   => "${softwareAutoInstall::params::configfile_owner}",
-        #     group   => "${softwareAutoInstall::params::configfile_group}",
-        #     mode    => "${softwareAutoInstall::params::configfile_mode}",
-        #     #notify  => Service['softwareAutoInstall'],
-        #     require => Package['softwareAutoInstall'],
-        # }
-        # # Populate the configuration file
-        # concat::fragment { "${softwareAutoInstall::params::configfile}_header":
-        #     target  => "${softwareAutoInstall::params::configfile}",
-        #     ensure  => "${softwareAutoInstall::ensure}",
-        #     content => template("softwareAutoInstall/softwareAutoInstall_header.conf.erb"),
-        #     #source => "puppet:///modules/softwareAutoInstall/softwareAutoInstall_header.conf",
-        #     order   => '01',
-        # }
-        # concat::fragment { "${softwareAutoInstall::params::configfile}_footer":
-        #     target  => "${softwareAutoInstall::params::configfile}",
-        #     ensure  => "${softwareAutoInstall::ensure}",
-        #     content => template("softwareAutoInstall/softwareAutoInstall_footer.conf.erb"),
-        #     #source => "puppet:///modules/softwareAutoInstall/softwareAutoInstall_footer.conf",
-        #     order   => '99',
-        # }
-
-        # PID file directory
-        # file { "${softwareAutoInstall::params::piddir}":
-        #     ensure  => 'directory',
-        #     owner   => "${softwareAutoInstall::params::piddir_user}",
-        #     group   => "${softwareAutoInstall::params::piddir_group}",
-        #     mode    => "${softwareAutoInstall::params::piddir_mode}",
-        # }
-
-        file { "${softwareAutoInstall::params::configfile_init}":
-            owner   => "${softwareAutoInstall::params::configfile_owner}",
-            group   => "${softwareAutoInstall::params::configfile_group}",
-            mode    => "${softwareAutoInstall::params::configfile_mode}",
-            ensure  => "${softwareAutoInstall::ensure}",
-            #content => template("softwareAutoInstall/default/softwareAutoInstall.erb"),
-            #source => "puppet:///modules/softwareAutoInstall/default/softwareAutoInstall.conf",
-            notify  =>  Service['softwareAutoInstall'],
-            require =>  Package['softwareAutoInstall']
-        }
-
-        service { 'softwareAutoInstall':
-            name       => "${softwareAutoInstall::params::servicename}",
-            enable     => true,
-            ensure     => running,
-            hasrestart => "${softwareAutoInstall::params::hasrestart}",
-            pattern    => "${softwareAutoInstall::params::processname}",
-            hasstatus  => "${softwareAutoInstall::params::hasstatus}",
-            require    => [
-                           Package['softwareAutoInstall'],
-                           File["${softwareAutoInstall::params::configfile_init}"]
-                           ],
-            subscribe  => File['softwareAutoInstall.conf'],
-        }
+      file { 'softwares.yaml':
+        ensure => present,
+        path   => '/tmp/softwares.yaml',
+        owner  => 'sw',
+        mode   => '0755',
+        source => 'puppet:///modules/softwareAutoInstall/softwares.yaml',
+      }
     }
-    else
-    {
-        # Here $softwareAutoInstall::ensure is 'absent'
-
-    }
-
 }
 
 
